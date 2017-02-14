ea68b0c76932fa14ca92e8adee5b533504dc5a9e
vitorrmorais <vitorrmorais@outlook.com>
2017-02-14 18:54:36 -0200

    Adicionado pontuacao por regiao

--- landing-page/receive.php
+++ landing-page/receive.php
@@ -2,14 +2,34 @@
 	include "conexao.php"; //adiciona arquivo de conexao com banco.
 
 	$nome     = $_POST["nome"];
-	$date	  = $_POST["date"]
-	$email	  = $_POST["email"]
-	$telefone = $_POST["telefone"]
-	$regiao	  = $_POST["regiao"]
-	$unidade  = $_POST["unidade"]	
-	
-	mysql_query("INSERT INTO dados(nome) VALUES('$nome')"); //envia os dados acima para o banco de dados.
-	
+	$date	  = $_POST["date"];
+	$email	  = $_POST["email"];
+	$telefone = $_POST["telefone"];
+	$regiao	  = $_POST["regiao"];
+	$unidade  = $_POST["unidade"];
+	$score = 10;
+	$token = "06bc8a9c285ef31334b63e60f7814d19";
 
+	switch ($regiao) { //Verifica a regiao e calcula os pontos
+    case "sul":
+        $score = $score - 2;
+	break;
+	case "sudeste";
+		if ($unidade != "São Paulo") { 
+			$score = $score - 1;
+		}
+	break;
+	case "centro-oeste";
+		$score = $score - 3;
+	break;
+	case "nordeste";
+		$score = $score - 4;
+	break;
+	case "norte";
+		$score = $score - 5;
+	break;
+	}
+		
+	
 	
 ?>
