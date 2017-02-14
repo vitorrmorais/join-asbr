89ed099792de05aa8304971da24ab950e24c48b1
vitorrmorais <vitorrmorais@outlook.com>
2017-02-14 18:24:36 -0200

    Adicionado arquivo de recebimento dos dados

---
+++ landing-page/receive.php
@@ -0,0 +1,15 @@
+﻿<?php
+	include "conexao.php"; //adiciona arquivo de conexao com banco.
+
+	$nome     = $_POST["nome"];
+	$date	  = $_POST["date"]
+	$email	  = $_POST["email"]
+	$telefone = $_POST["telefone"]
+	$regiao	  = $_POST["regiao"]
+	$unidade  = $_POST["unidade"]	
+	
+	mysql_query("INSERT INTO dados(nome) VALUES('$nome')"); //envia os dados acima para o banco de dados.
+	
+
+	
+?>
